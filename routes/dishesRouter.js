9dd4fc590ad433c349adfcb5d82ec4ca134ce115
StylishCS <joeshirf@gmail.com>
2023-08-19 16:42:56 +0300

    dishes

---
+++ routes/dishesRouter.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var router = express.Router();
+const { adminLogin } = require("../controllers/userController");
+const { validate } = require("../validation/login_validation");
+const { protect } = require("../middleware/protect");
+const {
+  addDish,
+  getAllDishes,
+  getDish,
+  updateDish,
+  deleteDish
+} = require("../controllers/dishesController");
+const upload = require("../middleware/uploadFiles");
+// router.use("/", validate());
+
+router
+  .route("/")
+  .get(getAllDishes)
+  .post(upload.single("image"), addDish)
+
+router.route("/:id")
+.get(getDish).patch(upload.single("image"), updateDish)
+.delete(deleteDish);
+;
+
+module.exports = router;
---
+++ services/reviewsServices.js
@@ -0,0 +1,6 @@
+const { connection } = require("../db/dbConnnection");
+const util = require("util");
+
+exports.getReviews = async () => {
+    //
+}
