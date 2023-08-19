d832f480335ba140f1f1c9458afd7d91330c262a
StylishCS <joeshirf@gmail.com>
2023-08-19 14:05:40 +0300

    admin login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const { adminLogin } = require("../controllers/userController");
+const {validate} = require('../validation/login_validation');
+const {protect} = require('../middleware/protect');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post("/login", validate(), adminLogin);
+
+// router.get('/', (req,res)=>{res.send('test')});
 
 module.exports = router;
